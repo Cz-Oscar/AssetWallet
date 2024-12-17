40df7b1d049afbe7dfafc37db80d5c0ad50c2a80
Oscar Czempiel <czempieloscar@gmail.com>
2024-12-17 18:40:46 +0100

    user now can add ivestment to database, resolved some issues

--- lib/pages/add_investement_page.dart
+++ lib/pages/add_investement_page.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import '../services/api_service.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AddInvestmentPage extends StatefulWidget {
   @override
@@ -17,6 +19,12 @@ class _AddInvestmentPageState extends State<AddInvestmentPage> {
   String? _selectedExchange;
   final TextEditingController _priceController =
       TextEditingController(); //price
+  final TextEditingController _cryptoController = TextEditingController();
+  final TextEditingController _exchangeController = TextEditingController();
+  final FocusNode _cryptoFocusNode = FocusNode();
+  final FocusNode _exchangeFocusNode = FocusNode();
+  bool _cryptoFieldActive = false;
+  bool _exchangeFieldActive = false;
 
   bool _isLoading = true;
 
@@ -54,6 +62,57 @@ class _AddInvestmentPageState extends State<AddInvestmentPage> {
     }
   }
 
+  Future<void> _addInvestmentToFirestore() async {
+    FocusScope.of(context).unfocus();
+
+    if (_selectedAsset == null ||
+        _selectedExchange == null ||
+        _priceController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Wypełnij wszystkie pola!")),
+      );
+      return;
+    }
+
+    final User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Nie jesteś zalogowany!")),
+      );
+      return;
+    }
+
+    try {
+      await FirebaseFirestore.instance
+          .collection('users') // Kolekcja użytkowników
+          .doc(user.uid) // Dokument użytkownika (po UID)
+          .collection('investments') // Podkolekcja inwestycji
+          .add({
+        'asset': _selectedAsset,
+        'exchange': _selectedExchange,
+        'price': double.tryParse(_priceController.text) ?? 0.0,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Inwestycja dodana pomyślnie!")),
+      );
+
+      setState(() {
+        _selectedAsset = null;
+        _selectedExchange = null;
+        _priceController.clear();
+        _cryptoController.clear();
+        _exchangeController.clear();
+      });
+    } catch (e) {
+      print("Błąd zapisu do Firestore: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Błąd zapisu: $e")),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,25 +121,35 @@ class _AddInvestmentPageState extends State<AddInvestmentPage> {
       ),
       body: _isLoading
           ? Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Wybierz kryptowalutę:', style: TextStyle(fontSize: 16)),
-                  SizedBox(height: 5),
-                  _buildCryptoTypeAheadField(),
-                  SizedBox(height: 20),
-                  Text('Wybierz giełdę:', style: TextStyle(fontSize: 16)),
-                  SizedBox(height: 5),
-                  _buildExchangeTypeAheadField(),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text('Podaj cene zakupu: ', style: TextStyle(fontSize: 16)),
-                  SizedBox(height: 5),
-                  _buildPriceFieldWithToolbar(),
-                ],
+          : SingleChildScrollView(
+              // Dodaj przewijanie
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Wybierz kryptowalutę:',
+                        style: TextStyle(fontSize: 16)),
+                    SizedBox(height: 5),
+                    _buildCryptoTypeAheadField(),
+                    SizedBox(height: 20),
+                    Text('Wybierz giełdę:', style: TextStyle(fontSize: 16)),
+                    SizedBox(height: 5),
+                    _buildExchangeTypeAheadField(),
+                    SizedBox(height: 20),
+                    Text('Podaj cenę zakupu:', style: TextStyle(fontSize: 16)),
+                    SizedBox(height: 5),
+                    _buildPriceFieldWithToolbar(),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        FocusScope.of(context).unfocus(); // Zamknij klawiaturę
+                        _addInvestmentToFirestore();
+                      },
+                      child: Text("Dodaj inwestycję do portfela"),
+                    ),
+                  ],
+                ),
               ),
             ),
     );
@@ -89,6 +158,9 @@ class _AddInvestmentPageState extends State<AddInvestmentPage> {
   /// Pole TypeAhead dla kryptowalut
   Widget _buildCryptoTypeAheadField() {
     return TypeAheadField<Map<String, dynamic>>(
+      controller: _cryptoController,
+      focusNode: _cryptoFocusNode,
+      hideOnEmpty: true,
       suggestionsCallback: (pattern) {
         return _allAssets.where((crypto) {
           final name = crypto['name']?.toLowerCase() ?? '';
@@ -98,16 +170,9 @@ class _AddInvestmentPageState extends State<AddInvestmentPage> {
         }).toList();
       },
       itemBuilder: (context, suggestion) {
-        final imageUrl = suggestion['image']; // Używamy właściwego klucza
         return ListTile(
-          leading: (imageUrl != null && imageUrl.isNotEmpty)
-              ? Image.network(
-                  imageUrl,
-                  width: 30,
-                  height: 30,
-                  errorBuilder: (context, error, stackTrace) =>
-                      Icon(Icons.error), // Obsługa błędu ładowania obrazu
-                )
+          leading: suggestion['image'] != null
+              ? Image.network(suggestion['image'], width: 30, height: 30)
               : Icon(Icons.image_not_supported),
           title: Text(suggestion['symbol']?.toUpperCase() ?? ''),
           subtitle: Text(suggestion['name'] ?? ''),
@@ -116,17 +181,29 @@ class _AddInvestmentPageState extends State<AddInvestmentPage> {
       onSelected: (suggestion) {
         setState(() {
           _selectedAsset = suggestion['name'];
+          _cryptoController.text = suggestion['symbol']?.toUpperCase() ?? '';
         });
-        print("Wybrano kryptowalutę: ${suggestion['name']}");
       },
       builder: (context, controller, focusNode) {
         return TextField(
           controller: controller,
           focusNode: focusNode,
+          readOnly: _cryptoFocusNode.hasFocus ? false : true,
           decoration: InputDecoration(
-            labelText: 'Szukaj kryptowaluty',
+            labelText: 'Wybierz kryptowalutę',
             border: OutlineInputBorder(),
             prefixIcon: Icon(Icons.search),
+            suffixIcon: _cryptoController.text.isNotEmpty
+                ? IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {
+                      setState(() {
+                        _cryptoController.clear();
+                        _selectedAsset = null;
+                      });
+                    },
+                  )
+                : null,
           ),
         );
       },
@@ -136,6 +213,9 @@ class _AddInvestmentPageState extends State<AddInvestmentPage> {
   /// Pole TypeAhead dla giełd
   Widget _buildExchangeTypeAheadField() {
     return TypeAheadField<Map<String, dynamic>>(
+      controller: _exchangeController,
+      focusNode: _exchangeFocusNode,
+      hideOnEmpty: true,
       suggestionsCallback: (pattern) {
         return _allExchanges.where((exchange) {
           final name = exchange['name']?.toLowerCase() ?? '';
@@ -144,15 +224,8 @@ class _AddInvestmentPageState extends State<AddInvestmentPage> {
       },
       itemBuilder: (context, suggestion) {
         return ListTile(
-          leading: suggestion['image'] != null &&
-                  (suggestion['image'] as String).isNotEmpty
-              ? Image.network(
-                  suggestion['image'],
-                  width: 30,
-                  height: 30,
-                  errorBuilder: (context, error, stackTrace) =>
-                      Icon(Icons.error),
-                )
+          leading: suggestion['image'] != null
+              ? Image.network(suggestion['image'], width: 30, height: 30)
               : Icon(Icons.image_not_supported),
           title: Text(suggestion['name'] ?? ''),
         );
@@ -160,17 +233,29 @@ class _AddInvestmentPageState extends State<AddInvestmentPage> {
       onSelected: (suggestion) {
         setState(() {
           _selectedExchange = suggestion['name'];
+          _exchangeController.text = suggestion['name'] ?? '';
         });
-        print("Wybrano giełdę: ${suggestion['name']}");
       },
       builder: (context, controller, focusNode) {
         return TextField(
           controller: controller,
           focusNode: focusNode,
+          readOnly: _exchangeFocusNode.hasFocus ? false : true,
           decoration: InputDecoration(
-            labelText: 'Szukaj giełdy',
+            labelText: 'Wybierz giełdę',
             border: OutlineInputBorder(),
             prefixIcon: Icon(Icons.search),
+            suffixIcon: _exchangeController.text.isNotEmpty
+                ? IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {
+                      setState(() {
+                        _exchangeController.clear();
+                        _selectedExchange = null;
+                      });
+                    },
+                  )
+                : null,
           ),
         );
       },
